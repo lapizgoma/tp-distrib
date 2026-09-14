9745e86ebcb0607ae4465c8f9a65ca90f420f680
Luca Lazarte <luca.lazarte05@gmail.com>
2026-09-14 12:31:09 -0300

    chore(rest): add Spring backend for REST services

---
+++ backend-rest/src/main/java/com/tp_distribuidos/backend_rest/BackendRestApplication.java
@@ -0,0 +1,13 @@
+package com.tp_distribuidos.backend_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendRestApplication.class, args);
+	}
+
+}
---
+++ backend-rest/src/test/java/com/tp_distribuidos/backend_rest/BackendRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tp_distribuidos.backend_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
